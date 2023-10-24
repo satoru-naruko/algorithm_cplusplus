8d6650bab434cea9b27d98aad666c714990da4cf
satoru.naruko <naruko505@gmail.com>
2023-10-24 23:18:03 +0900

    fix build error for template T

--- include/linked_list.h
+++ include/linked_list.h
@@ -1,76 +1,75 @@
-#pragma once
+#pragma once // include guard
 
 using namespace std;
-
 template<class T>
 class LinkedList {
 private:
-	template<class T>
-	class LinkedListNode {
-	public:
-		T key{};
-		LinkedListNode* prev = nullptr;
-		LinkedListNode* next = nullptr;
-	};
-	LinkedListNode<T> front;
-	int insertImpl(T& insertkey) {
-		auto newNode = new LinkedListNode<T>();
-		if (newNode) {
-			newNode->key = insertkey;
-			newNode->next = front.next;
-			front.next->prev = newNode;
-			front.next = newNode;
-			newNode->prev = &front;
-		}
-		else {
-			return -1;
-		}
+    struct Node {
+        T data;
+        Node* prev;
+        Node* next;
+        Node() : prev(nullptr), next(nullptr) {}
+        Node(T value) : data(value), prev(nullptr), next(nullptr) {}
+    };
+    Node front;
+    int size;
 
-		return 0;
-	}
+    Node* search(T& searchKey) {
+        Node* p = front.next;
+        while (p != &front) {
+            if (p->data == searchKey) {
+                return p;
+            }
+            p = p->next;
+        }
+        return &front;
+    }
 
-	LinkedListNode<T>* search(T searchKey) {
-		auto pCurrent = front.next;
-		while (pCurrent != &front && pCurrent->key != searchKey)
-		{
-			pCurrent = pCurrent->next;
-		}
+    int insertImpl(T& insertKey) {
+        Node* p = search(insertKey);
+        if (p != &front) {
+            return 0;
+        }
+        Node* newNode = new Node(insertKey);
+        newNode->next = &front;
+        newNode->prev = front.prev;
+        front.prev->next = newNode;
+        front.prev = newNode;
+        size++;
+        return 1;
+    }
 
-		return pCurrent;
-	}
 public:
-	LinkedList() {
-		front.next = &front;
-		front.prev = &front;
-	}
+    LinkedList() : size(0) {
+        front.next = &front;
+        front.prev = &front;
+    }
 
-	int insert(T& insertkey) {
-		return insertImpl(insertkey);
-	}
+    int insert(T& insertKey) {
+        return insertImpl(insertKey);
+    }
 
-	int insert(T insertkey) {
-		return insertImpl(insertkey);
-	}
+    int insert(T insertKey) {
+        return insertImpl(insertKey);
+    }
 
-	void deleteNode(T &deleteKey) {
-		auto p = search(deleteKey);
-		if (p == &front) {
-			return;
-		}
-		p->prev->next = p->next;
-		p->next->prev = p->prev;
-		delete p; p = nullptr;
-	}
+    void deleteNode(T& deleteKey) {
+        auto p = search(deleteKey);
+        if (p == &front) {
+            return;
+        }
+        p->prev->next = p->next;
+        p->next->prev = p->prev;
+        delete p; p = nullptr;
+    }
 
-	void deleteNode(T deleteKey) {
-		auto p = search(deleteKey);
-		if (p == &front) {
-			return;
-		}
-		p->prev->next = p->next;
-		p->next->prev = p->prev;
-		delete p; p = nullptr;
-	}
-
+    void deleteNode(T deleteKey) {
+        auto p = search(deleteKey);
+        if (p == &front) {
+            return;
+        }
+        p->prev->next = p->next;
+        p->next->prev = p->prev;
+        delete p; p = nullptr;
+    }
 };
-
